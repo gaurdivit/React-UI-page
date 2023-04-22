80ae0fd397ea50fc91084b4bff3a1c30fc7f0596
divit gaur <divitgaur@divits-MacBook-Pro.local>
2023-04-22 23:07:48 +0530

    git push

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from "react";
+
+import CTA from "../components/CTA";
+import InfoCard from "../components/Cards/InfoCard";
+import ChartCard from "../components/Chart/ChartCard";
+import { Doughnut, Line } from "react-chartjs-2";
+import ChartLegend from "../components/Chart/ChartLegend";
+import PageTitle from "../components/Typography/PageTitle";
+import { ChatIcon, CartIcon, MoneyIcon, PeopleIcon } from "../icons";
+import RoundIcon from "../components/RoundIcon";
+import response from "../utils/demo/tableData";
+import {
+  TableBody,
+  TableContainer,
+  Table,
+  TableHeader,
+  TableCell,
+  TableRow,
+  TableFooter,
+  Avatar,
+  Badge,
+  Pagination,
+} from "@windmill/react-ui";
+
+import {
+  doughnutOptions,
+  lineOptions,
+  doughnutLegends,
+  lineLegends,
+} from "../utils/demo/chartsData";
+
+function Dashboard() {
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([]);
+
+  // pagination setup
+  const resultsPerPage = 10;
+  const totalResults = response.length;
+
+  // pagination change control
+  function onPageChange(p) {
+    setPage(p);
+  }
+
+  // on page change, load new sliced data
+  // here you would make another server request for new data
+  useEffect(() => {
+    setData(response.slice((page - 1) * resultsPerPage, page * resultsPerPage));
+  }, [page]);
+
+  const initialState = [
+    {
+      id: "1",
+      title: "kush",
+      amount: "200",
+      type: "income",
+      category: "food",
+    },
+    {
+      id: "1",
+      title: "kush",
+      amount: "2000",
+      type: "incomewer",
+      category: "foodfsd",
+    },
+  ];
+
+  const [users, setUsers] = useState(initialState);
+
+  async function getApiData() {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/",
+      {
+        headers: {
+          "x-access-token": localStorage.getItem("ResponseToken"),
+        },
+      }
+    ).then((response) => {
+      // do something
+      parseFloat(response.headers.get("X-WP-TotalPages"));
+    });
+
+    // update the state
+    setUsers((users) => [response]);
+  }
+
+  return (
+    <>
+      <PageTitle>Dashboard</PageTitle>
+
+      <CTA />
+
+      {/* <!-- Cards --> */}
+      <div className="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        {users.map((user) => (
+          <div className="user" key={user.id}>
+            <InfoCard title="Title" value={user.title}>
+              <RoundIcon
+                icon={PeopleIcon}
+                iconColorClass="text-orange-500 dark:text-orange-100"
+                bgColorClass="bg-orange-100 dark:bg-orange-500"
+                className="mr-4"
+              />
+            </InfoCard>
+            <InfoCard title="Amount" value={user.amount}>
+              <RoundIcon
+                icon={MoneyIcon}
+                iconColorClass="text-green-500 dark:text-green-100"
+                bgColorClass="bg-green-100 dark:bg-green-500"
+                className="mr-4"
+              />
+            </InfoCard>
+            <InfoCard title="Type" value={user.type}>
+              <RoundIcon
+                icon={CartIcon}
+                iconColorClass="text-blue-500 dark:text-blue-100"
+                bgColorClass="bg-blue-100 dark:bg-blue-500"
+                className="mr-4"
+              />
+            </InfoCard>
+            <InfoCard title="Category" value={user.category}>
+              <RoundIcon
+                icon={ChatIcon}
+                iconColorClass="text-teal-500 dark:text-teal-100"
+                bgColorClass="bg-teal-100 dark:bg-teal-500"
+                className="mr-4"
+              />
+            </InfoCard>
+          </div>
+        ))}
+      </div>
+
+      <TableContainer>
+        <Table>
+          <TableHeader>
+            <tr>
+              <TableCell>Client</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Date</TableCell>
+            </tr>
+          </TableHeader>
+          <TableBody>
+            {users.map((user, i) => (
+              <TableRow key={i}>
+                <TableCell>
+                  <div className="flex items-center text-sm">
+                    <Avatar
+                      className="hidden mr-3 md:block"
+                      src={user.avatar}
+                      alt="User image"
+                    />
+                    <div>
+                      <p className="font-semibold">{user.name}</p>
+                      <p className="text-xs text-gray-600 dark:text-gray-400">
+                        {user.job}
+                      </p>
+                    </div>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">$ {user.amount}</span>
+                </TableCell>
+                <TableCell>
+                  <Badge type={user.status}>{user.status}</Badge>
+                </TableCell>
+                <TableCell>
+                  <span className="text-sm">
+                    {new Date(user.date).toLocaleDateString()}
+                  </span>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <TableFooter>
+          <Pagination
+            totalResults={totalResults}
+            resultsPerPage={resultsPerPage}
+            label="Table navigation"
+            onChange={onPageChange}
+          />
+        </TableFooter>
+      </TableContainer>
+
+      <PageTitle>Charts</PageTitle>
+      <div className="grid gap-6 mb-8 md:grid-cols-2">
+        <ChartCard title="Revenue">
+          <Doughnut {...doughnutOptions} />
+          <ChartLegend legends={doughnutLegends} />
+        </ChartCard>
+
+        <ChartCard title="Traffic">
+          <Line {...lineOptions} />
+          <ChartLegend legends={lineLegends} />
+        </ChartCard>
+      </div>
+    </>
+  );
+}
+
+export default Dashboard;
